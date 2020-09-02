6de91f5bc34ddb6bf532964e446e7ee94f5d5da8
MarkNewman-github  <marknewmanmail@163.com>
2020-09-02 10:00:49 +0800

    test

--- src/S01/Test01.java
+++
@@ -1,7 +0,0 @@
-package S01;
-
-public class Test01 {
-    public static void main(String[] args) {
-        System.out.printf("mark");
-    }
-}
---
+++ src/com/mark/basic/s01/Fib.java
@@ -0,0 +1,19 @@
+package com.mark.basic.s01;
+
+public class Fib {
+
+    /**
+     * 求斐波那契数列的第n项
+     * 0，1，1，2，3，5，8，13....
+     */
+
+    public static int fib(int n){
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        fib(10);
+    }
+}
