ef7a01dddf581de7fda9c425b72d095fbad888ae
MarkNewman-github  <marknewmanmail@163.com>
2020-09-04 12:26:41 +0800

    test

--- src/com/mark/basic/s01/Fib.java
+++ src/com/mark/basic/s01/Fib.java
@@ -7,13 +7,51 @@ public class Fib {
      * 0，1，1，2，3，5，8，13....
      */
 
+    /**
+     * 方法1：
+     * 采用递归的方式进行计算，时间复杂度
+     * @param n
+     * @return
+     */
     public static int fib(int n){
 
-        return 0;
+        if (n <= 1) {
+            return n;
+        }
+        int num = fib(n-1) + fib(n-2);
+
+        return num;
+    }
+
+
+    /**
+     * 方法2：
+     * 采用循环的方式获取n-1和n-2
+     * @param n
+     */
+    public static int fib2(int n){
+
+        if (n <= 1){
+            return n;
+        }
+
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < n-1; i++) {
+
+            int sum = first + second;
+            first = second;
+            second = sum;
+
+        }
+
+        return second;
     }
 
 
     public static void main(String[] args) {
-        fib(10);
+        int n = fib2(10);
+        System.out.print(n);
     }
 }
