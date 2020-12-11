f53ce8b5b8025bc00b685f1b034ba669080407c1
MarkNewman-github  <marknewmanmail@163.com>
2020-12-11 12:40:20 +0800

    update

---
+++ src/com/mark/basic/leetcode/IsBalanced_110.java
@@ -0,0 +1,28 @@
+package com.mark.basic.leetcode;
+
+import com.mark.basic.utils.TreeNode;
+
+/**
+ * @author mark
+ *
+ * 110. 平衡二叉树
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过
+ *
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ *
+ * 链接：https://leetcode-cn.com/problems/balanced-binary-tree/
+ *
+ */
+public class IsBalanced_110 {
+
+    public boolean isBalanced(TreeNode root) {
+
+
+        return false;
+    }
+}
---
+++ src/com/mark/basic/utils/TreeNode.java
@@ -0,0 +1,19 @@
+package com.mark.basic.utils;
+
+
+/**
+ * leetcode树形练习题目通用类
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+      }
+}
