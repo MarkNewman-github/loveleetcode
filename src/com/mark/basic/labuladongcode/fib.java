0ad87615d105d09fa96a634c8c9f9fb6804f36c1
mark <marknewmanmail@163.com>
2020-12-24 13:14:49 +0800

    update

--- src/com/mark/basic/labuladongcode/fib.java
+++ src/com/mark/basic/labuladongcode/fib.java
@@ -4,5 +4,19 @@ package com.mark.basic.labuladongcode;
  * 斐波那契数列
  */
 public class fib {
+
+
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(fibTest(n));
+    }
+
+    public static int fibTest(int n){
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibTest(n-1) + fibTest(n-2);
+    }
     
 }
