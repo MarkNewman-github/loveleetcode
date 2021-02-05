b5fd15bbaded9df64555ea394cf8f1c88009a57a
MarkNewman-github  <marknewmanmail@163.com>
2021-02-05 21:14:59 +0800

    updaete

--- src/com/mark/basic/leetcode/FirstQuestionTwoSum.java
+++ src/com/mark/basic/leetcode/FirstQuestionTwoSum.java
@@ -1,8 +1,5 @@
 package com.mark.basic.leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * 
@@ -17,7 +14,8 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
-public class TwoSum_1 {
+
+public class FirstQuestionTwoSum {
 
     public static void main(String[] args) {
         int[] num = {2, 7, 11, 15};
@@ -40,4 +38,5 @@ public class TwoSum_1 {
         }
         return new int[0];
     }
+    
 }
---
+++ src/com/mark/basic/utils/ListNode.java
@@ -0,0 +1,13 @@
+package com.mark.basic.utils;
+
+/**
+ * leetcode的工具类
+ */
+public class ListNode {
+    
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
